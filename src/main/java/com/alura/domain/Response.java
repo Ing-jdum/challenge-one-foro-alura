2c23b0362acf809bf9e76854c34360d9c3ae5416
ing.jdum <ing.jdum@gmail.com>
2023-09-14 12:19:31 -0400

    package restructure

--- src/main/java/com/alura/domain/Response.java
+++ src/main/java/com/alura/domain/Response.java
@@ -2,7 +2,7 @@ package com.alura.domain;
 
 import java.time.LocalDateTime;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Retry.Topic;
+import com.alura.domain.topic.Topic;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -17,94 +17,94 @@ import jakarta.persistence.Table;
 @Entity(name = "Response")
 public class Response {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String message;
+	private String message;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "topic_id")
-    private Topic topic;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
 
-    private LocalDateTime creationDate = LocalDateTime.now();
+	private LocalDateTime creationDate = LocalDateTime.now();
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User author;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User author;
 
-    private Boolean solution = false;
+	private Boolean solution = false;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Response other = (Response) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Response other = (Response) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public Topic getTopic() {
-        return topic;
-    }
+	public Topic getTopic() {
+		return topic;
+	}
 
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
 
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
 
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
 
-    public User getAuthor() {
-        return author;
-    }
+	public User getAuthor() {
+		return author;
+	}
 
-    public void setAuthor(User author) {
-        this.author = author;
-    }
+	public void setAuthor(User author) {
+		this.author = author;
+	}
 
-    public Boolean getSolution() {
-        return solution;
-    }
+	public Boolean getSolution() {
+		return solution;
+	}
 
-    public void setSolution(Boolean solution) {
-        this.solution = solution;
-    }
+	public void setSolution(Boolean solution) {
+		this.solution = solution;
+	}
 }
